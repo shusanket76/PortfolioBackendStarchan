43825cf06b7b27e802f374dd3c1aad18b6e21550
Shusanket <shusanketbasyal76@gmail.com>
2023-07-03 22:24:19 -0400

    cors

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5174",
   "http://localhost:5173",
+  "https://portfolio-frontend-starchan.vercel.app",
 ];
 // ================================================================================================
 
